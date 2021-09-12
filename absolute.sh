1e3d8a7e999dc762d5c19d6bb6d02316042d8d1d
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-12 20:44:50 +0300

    Update absolute.sh

--- absolute.sh
+++ absolute.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir /tmp/'hello world'/
+mkdir /tmp/'hello world'
 echo hello world\!\! > /tmp/'hello world'/absolute.txt
 cat /tmp/'hello world'/absolute.txt 
+ls -l /tmp/'hello world'/absolute.txt 
