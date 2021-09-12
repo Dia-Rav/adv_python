c0c2bb2b3d88d8b89cfe7d1830cef5dc28d40f81
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-12 20:48:07 +0300

    Update current.sh

--- current.sh
+++ current.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir ./'hello current'/
-echo hello current\!\! > ./'hello current'/current.txt
-cat ./'hello current'/current.txt
+mkdir 'hello current'
+echo hello current\!\! > 'hello current'/current.txt
+cat 'hello current'/current.txt
+ls -l 'hello current'/current.txt
