6edb30e6b16bf19fd1885d2c7395146f1b89c49c
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-17 14:51:38 +0300

    Update task_1.py

--- lab2_work_with_file/task_1.py
+++ lab2_work_with_file/task_1.py
@@ -1,7 +1,7 @@
 
-with open("1task.txt", "w") as file:
+with open("task_1_output.txt", "w") as file:
     file.write("rgn4c ckjnr2   \n" + "cm23n1j \n" + "HELLO")
-with open("1task.txt", "r") as file:
+with open("task_1_output.txt", "r") as file:
     for line in file:
         print(line.strip()) 
        
