0276ce008672b0738852541016ea056abda2c32e
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-17 20:11:34 +0300

    Create task_3.py

---
+++ lab2_work_with_file/task_3.py
@@ -0,0 +1,14 @@
+import zipfile
+import os
+archive = zipfile.ZipFile('main.zip', 'r')
+archive.extractall('.')
+archive.close()
+ans = []
+for current_dir, dirs, files in os.walk("main"): #передаем в качестве аргумента текущую директорию
+    for file in files:
+        if file.find('.py')>0:
+            answer = current_dir.split('\\')
+            ans.append (answer[-1])
+            break
+ans = sorted(ans)
+print (ans)
