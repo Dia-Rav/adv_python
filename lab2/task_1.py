0c7a030973e3d3031ab5b636047f540773c80737
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-17 14:45:05 +0300

    Create task_1.py

---
+++ lab2/task_1.py
@@ -0,0 +1,6 @@
+
+with open("1task", "w") as file:
+    file.write("rgn4c ckjnr2   \n" + "cm23n1j \n" + "HELLO")
+with open("1task", "r") as file:
+    for line in file:
+        print(line.strip()) 
