849475e3a2b9a17d4109840d79705dc61f568c07
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-09-25 19:00:58 +0300

    Add files via upload

---
+++ lab3_work_with_arguments/decorator_with_arg.py
@@ -0,0 +1,14 @@
+def swap (func):
+    def new_div(*arguments,  **show):
+        arg = reversed(arguments)
+        func(*arg, **show )
+    return new_div
+
+@swap
+def div(x, y, show=False):
+    res = x / y
+    if show:
+        print(res)
+    return res
+
+print(div(10, 5, show=True))
