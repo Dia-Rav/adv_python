3f195defede27269dfb76d12cb418d5b9b1f1bee
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-10-03 09:31:03 +0300

    Add files via upload

---
+++ lab4_OOP/task_2.2_daugther_mother.py
@@ -0,0 +1,14 @@
+
+class mother():
+    def __str__(self):
+        return self.who_am_i()
+    def who_am_i(self):
+        return 'I am mother'
+class daugther(mother):
+    def who_am_i(self):
+        return 'I am daugther'
+
+M = mother()
+D = daugther()
+print (M)
+print (D)
