dd6b7b6014cf6aa2e0915c399b96cdebac21a085
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2021-11-11 15:33:27 +0300

    Add files via upload

---
+++ lab10_threads/rewrite_programm.py
@@ -0,0 +1,34 @@
+import urllib.request
+import time
+import threading
+
+
+urls = [
+    'https://www.yandex.ru', 'https://www.google.com',
+    'https://habrahabr.ru', 'https://www.python.org',
+    'https://isocpp.org',
+]
+
+
+def read_url(url):
+    with urllib.request.urlopen(url) as u:
+        return u.read()
+
+start = time.time()
+threads = [threading.Thread(target=read_url(url)) for url in urls]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+print('c потоками: ', time.time() - start) 
+
+start = time.time()
+for url in urls:
+    read_url(url)
+print('без: ', time.time() - start)
+
+'''
+c потоками:  4.377910137176514
+без:  3.661693572998047
+'''
+
